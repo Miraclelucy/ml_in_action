2de0d8baff6980c472141f758f05cae9708d67a4
藤露 <080301087@163.com>
2021-06-04 18:41:14 +0800

    Update tree_0_draw_0.py

--- ch03-Decision-Tree/basic/tree_0_draw_0.py
+++ ch03-Decision-Tree/basic/tree_0_draw_0.py
@@ -15,10 +15,10 @@ def calc_shannonent(dataset):  # 计算熵
     for featvec in dataset:
         currentlabel = featvec[-1]
         if currentlabel not in labelcounts.keys(): labelcounts[currentlabel] = 0
-        labelcounts[currentlabel] += 1
+        labelcounts[currentlabel] += 1  # 统计每个标签的数据总数
     shannonent = 0.0
     for key in labelcounts:
-        prob = float(labelcounts[key]) / numentries
+        prob = float(labelcounts[key]) / numentries  # 每个标签的数据总数/所有数据总数
         shannonent -= prob * log(prob, 2)  # 对数函数
     return shannonent
 
@@ -26,27 +26,28 @@ def calc_shannonent(dataset):  # 计算熵
 def split_dataset(dataset, axis, value):
     retdataset = []
     for featvec in dataset:
-        if featvec[axis] == value:
+        if featvec[axis] == value:  # 统计某一条件下的子数据集
             reducedfeatvec = featvec[:axis]
-            reducedfeatvec.extend(featvec[axis + 1:])
+            reducedfeatvec.extend(featvec[axis + 1:])  # 把除了特征值value之外的其他特征值拼接起来
             retdataset.append(reducedfeatvec)
     return retdataset
 
 
 def choose_bestfeature_tosplit(dataset):
     numfeatures = len(dataset[0]) - 1  # 最后一列是标签
-    baseentropy = calc_shannonent(dataset)
+    baseentropy = calc_shannonent(dataset)  # 计算信息熵
     bestinfogain = 0.0
     bestfeature = -1
     for i in range(numfeatures):
-        featlist = [example[i] for example in dataset]
-        uniquevals = set(featlist)
+        featlist = [example[i] for example in
+                    dataset]  # 数据集所有行的第i个特征，比如i=0时，值为列表['晴', '晴', '阴', '雨', '雨', '雨', '阴', '晴', '晴', '雨', '晴', '阴', '阴', '雨']
+        uniquevals = set(featlist)  # 列举第i个特征的所有值，比如i=0时，值为集合{'晴', '阴', '雨'}
         newentropy = 0.0
-        for value in uniquevals:
-            subdataset = split_dataset(dataset, i, value)
+        for value in uniquevals:  # 遍历特征集合中的所有值
+            subdataset = split_dataset(dataset, i, value)  # 获取某一条件下的子数据集
             prob = len(subdataset) / float(len(dataset))
-            newentropy += prob * calc_shannonent(subdataset)
-        infogain = baseentropy - newentropy  # 计算信息增益
+            newentropy += prob * calc_shannonent(subdataset)  # 计算条件熵
+        infogain = baseentropy - newentropy  # 计算信息增益 = 信息熵- 条件熵
         if infogain > bestinfogain:
             bestinfogain = infogain
             bestfeature = i
@@ -63,13 +64,13 @@ def majority_cnt(classlist):
 
 
 def createtree(dataset, labels):  # 生成决策树
-    classlist = [example[-1] for example in dataset]
+    classlist = [example[-1] for example in dataset]  # 取出标签
     if classlist.count(classlist[0]) == len(classlist):
         return classlist[0]  # 当所有数据的类别都一样的是时候停止分裂
     if len(dataset[0]) == 1:
         return majority_cnt(classlist)  # 当没有更多特征的时候停止分裂
     bestfeat = choose_bestfeature_tosplit(dataset)
-    bestfeatlabel = labels[bestfeat]
+    bestfeatlabel = labels[bestfeat]  # 获取最好的特征对应的值
     mytree = {bestfeatlabel: {}}
     del (labels[bestfeat])
     featvalues = [example[bestfeat] for example in dataset]
@@ -114,7 +115,7 @@ def gettreedepth(mytree):
             thisdepth = 1 + gettreedepth(seconddict[key])
         else:
             thisdepth = 1
-        if thisdepth > maxdepth : maxdepth = thisdepth
+        if thisdepth > maxdepth: maxdepth = thisdepth
     return maxdepth
 
 
@@ -168,6 +169,7 @@ def createplot(intree):
 if __name__ == "__main__":
     fr = open('tennis.txt', encoding='UTF-8')  # 读取训练数据
     trainingdata = [inst.strip().split('\t') for inst in fr.readlines()]
+    print(type(trainingdata[0]))
     labels = ['天气', '气温', '湿度', '风力']  # 特性的名称
     decisiontree = createtree(trainingdata, labels)  # 生成决策树
     createplot(decisiontree)  # 图示决策树
